9d227f072d67c1521175b92b6f1e05c20a082950
joana-nishimura-narazaki <joana.narazaki45@gmail.com>
2025-06-23 20:21:18 -0300

    Descrição do que foi alterado

---
+++ frontend/appointments-ui/src/app/features/consultas/consultas-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConsultasRoutingModule } from './consultas-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ConsultasRoutingModule
+  ]
+})
+export class ConsultasModule { }
